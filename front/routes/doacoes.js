eb3b26b4c8c8f8841309d4f1d67d537c1a33d9e3
Ramos <joao.lucas.c.ramos@accenture.com>
2024-06-20 12:10:04 -0300

    adicionando view de doador

--- front/routes/doacoes.js
+++ front/routes/doacoes.js
@@ -1,35 +1,36 @@
 var express = require('express');
 var router = express.Router();
-const url = 'http://localhost:4000/doador';
+const url = 'http://localhost:4000/doacoes';
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   fetch(url, { method: 'GET' })
-    .then(async (res) => {
-      if (!res.ok) {
-        const err = await res.json();
-        throw err;
-      }
-      return res.json();
-    })
-    .then((doadores) => {
-      let title = 'Gestão de Doadores';
-      let cols = ["Id", "Nome", "Email", "Endereço", "Telefone", "Ações"];
-      res.render('layout', { body: 'pages/doador',title, doadores, cols, error: "" });
-    })
-    .catch((error) => {
-      console.log('Erro', error);
-      res.render( 'layout' , { body: 'pages/doador',title :"gestâo de Doadores"});
-    });
+  .then(async (res) =>{
+    if (!res.ok) {
+      const err = await res.json();
+      throw err;
+    }
+    return res.json();
+  })
+  .then((doacoes) => {
+    let title = 'Gestão de Doações';
+    let cols = ["Id", "Descrição", "Data", "Doação", "Ações"];
+    res.render('layout', { body: 'pages/doacao', title, doacoes, cols, error: "" });
+  })
+  .catch((error) => {
+    console.log('Erro', error);
+    res.render('layout', { body: 'pages/doacao', title: "Gestão de Doações", error: "Erro ao buscar doações" });
+  });
 });
 
-// Criar novo doador
+// Post Nova Doação
 router.post("/", (req, res) => {
-  const { nome, email, endereco, telefone } = req.body;
+  const { descricao, data, doacao } = req.body;
   fetch(url, {
     method: "POST",
     headers: {"Content-Type": "application/json"},
-    body: JSON.stringify({ nome, email, endereco, telefone })
+    body: JSON.stringify({ descricao, data, doacao })
   }).then(async (res) => {
     if (!res.ok) {
       const err = await res.json();
@@ -37,22 +38,22 @@ router.post("/", (req, res) => {
     }
     return res.json();
   })
-  .then((doador) => {
-    res.send(doador);
+  .then((doacao) => {
+    res.send(doacao);
   })
   .catch((error) => {
     res.status(500).send(error);
   });
 });
 
-// Atualizar doador por ID
+// Atualizando completamente a Doação
 router.put("/:id", (req, res) => {
   const { id } = req.params;
-  const { nome, email, endereco, telefone } = req.body;
-  fetch(`${url}/${id}`, {
+  const { descricao, data, doacao } = req.body;
+  fetch(url + '/' + id, {
     method: "PUT",
     headers: {"Content-Type": "application/json"},
-    body: JSON.stringify({ nome, email, endereco, telefone })
+    body: JSON.stringify({ descricao, data, doacao })
   }).then(async (res) => {
     if (!res.ok) {
       const err = await res.json();
@@ -60,18 +61,18 @@ router.put("/:id", (req, res) => {
     }
     return res.json();
   })
-  .then((doador) => {
-    res.send(doador);
+  .then((doacao) => {
+    res.send(doacao);
   })
   .catch((error) => {
     res.status(500).send(error);
   });
 });
 
-// Deletar doador por ID
+// Deletar Doação
 router.delete("/:id", (req, res) => {
   const { id } = req.params;
-  fetch(`${url}/${id}`, {
+  fetch(url + '/' + id, {
     method: "DELETE"
   }).then(async (res) => {
     if (!res.ok) {
@@ -80,18 +81,18 @@ router.delete("/:id", (req, res) => {
     }
     return res.json();
   })
-  .then((doador) => {
-    res.send(doador);
+  .then((doacao) => {
+    res.send(doacao);
   })
   .catch((error) => {
     res.status(500).send(error);
   });
 });
 
-// Buscar doador por ID
+// Buscando Doação por ID
 router.get("/:id", (req, res) => {
   const { id } = req.params;
-  fetch(`${url}/${id}`, {
+  fetch(url + '/' + id, {
     method: "GET"
   }).then(async (res) => {
     if (!res.ok) {
@@ -100,8 +101,8 @@ router.get("/:id", (req, res) => {
     }
     return res.json();
   })
-  .then((doador) => {
-    res.send(doador);
+  .then((doacao) => {
+    res.send(doacao);
   })
   .catch((error) => {
     res.status(500).send(error);
